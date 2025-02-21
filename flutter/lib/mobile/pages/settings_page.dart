e588fbb78016079cfec2b7091637f57ead2d6799
Northernn <67755436+Northernn@users.noreply.github.com>
2025-02-21 16:14:36 +0000

    Update settings_page.dart

--- flutter/lib/mobile/pages/settings_page.dart
+++ flutter/lib/mobile/pages/settings_page.dart
@@ -787,7 +787,7 @@ class _SettingsState extends State<SettingsPage> with WidgetsBindingObserver {
                 title: Text(translate("Version: ") + version),
                 value: Padding(
                   padding: EdgeInsets.symmetric(vertical: 8),
-                  child: Text('rustdesk.com',
+                  child: Text('www.brunelcomputing.co.uk',
                       style: TextStyle(
                         decoration: TextDecoration.underline,
                       )),
@@ -812,7 +812,7 @@ class _SettingsState extends State<SettingsPage> with WidgetsBindingObserver {
             SettingsTile(
               title: Text(translate("Privacy Statement")),
               onPressed: (context) =>
-                  launchUrlString('https://rustdesk.com/privacy.html'),
+                  launchUrlString('https://www.brunelcomputing.co.uk/privacy.html'),
               leading: Icon(Icons.privacy_tip),
             )
           ],
